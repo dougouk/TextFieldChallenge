749a38dcecf43bf52dda591e26610d82b347527f
Dongoug Kim <dougouk@gmail.com>
2017-02-24 13:23:26 -0500

    Initial Commit

---
+++ TextFieldChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TextFieldChallenge
+//
+//  Created by Dan on 2/24/17.
+//  Copyright © 2017 Dan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
